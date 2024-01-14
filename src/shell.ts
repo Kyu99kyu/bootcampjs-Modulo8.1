1868264981eeda3ac83cc9c71acbb4a2e9c06b49
sazo <sazdev09@gmail.com>
2024-01-14 20:11:04 +0100

    feat: Actualizar Shell para incluir activación de protocolo de urgencia
    Se ha actualizado la clase 'Shell' para incluir la ejecución de la función 'activarProtocoloUrgencia' con datos de ejemplo. Ahora se verifica si se debe activar el protocolo de urgencia junto con las demás funciones existentes.

--- src/shell.ts
+++ src/shell.ts
@@ -1,7 +1,9 @@
-import {obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios} from "./motor";
+import {obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios, activarProtocoloUrgencia} from "./motor";
 import {pacientes} from "./modelo";
 
 const listaDePacientes = obtenPacientesAsignadosAPediatria(pacientes);
 const resultadoMenores10 = obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes);
+const activarProtocolo = activarProtocoloUrgencia(pacientes);
 console.log({listaDePacientes});
 console.log({resultadoMenores10})
+console.log({activarProtocolo})
