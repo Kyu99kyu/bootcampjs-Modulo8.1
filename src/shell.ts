86b8b647139c2f2743f9328175c6e151fac1b9bd
sazo <sazdev09@gmail.com>
2024-01-14 21:03:17 +0100

    feat: Actualizar Shell para incluir verificación de pacientes de Pediatría
    Se ha actualizado la clase 'Shell' para importar la función 'HayPacientesDePediatria' y ejecutarla junto con las demás funciones existentes. Ahora se verifica la presencia de pacientes de la especialidad de Pediatría en la lista proporcionada.

--- src/shell.ts
+++ src/shell.ts
@@ -1,9 +1,12 @@
-import {obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios, activarProtocoloUrgencia} from "./motor";
+import {obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios, activarProtocoloUrgencia,HayPacientesDePediatria} from "./motor";
 import {pacientes} from "./modelo";
 
 const listaDePacientes = obtenPacientesAsignadosAPediatria(pacientes);
 const resultadoMenores10 = obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes);
 const activarProtocolo = activarProtocoloUrgencia(pacientes);
+const finDeTurno = HayPacientesDePediatria(pacientes);
+
 console.log({listaDePacientes});
 console.log({resultadoMenores10})
 console.log({activarProtocolo})
+console.log({finDeTurno});
