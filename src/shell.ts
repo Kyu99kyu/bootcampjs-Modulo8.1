4d1aa59b6ce0a0108cb388a57fd33fbfb204820b
sazo <sazdev09@gmail.com>
2024-01-14 21:24:25 +0100

    feat: Actualizar Shell para incluir conteo de pacientes por especialidad
    Se ha actualizado la clase 'Shell' para importar y ejecutar la función 'cuentaPacientesPorEspecialidad' junto con las demás funciones existentes. Ahora se obtiene el conteo de pacientes por especialidad.

--- src/shell.ts
+++ src/shell.ts
@@ -1,12 +1,14 @@
-import {obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios, activarProtocoloUrgencia,HayPacientesDePediatria} from "./motor";
+import {obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios, activarProtocoloUrgencia,HayPacientesDePediatria,cuentaPacientesPorEspecialidad} from "./motor";
 import {pacientes} from "./modelo";
 
 const listaDePacientes = obtenPacientesAsignadosAPediatria(pacientes);
 const resultadoMenores10 = obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes);
 const activarProtocolo = activarProtocoloUrgencia(pacientes);
 const finDeTurno = HayPacientesDePediatria(pacientes);
+const listaDeEspecialistas = cuentaPacientesPorEspecialidad(pacientes);
 
 console.log({listaDePacientes});
 console.log({resultadoMenores10})
 console.log({activarProtocolo})
 console.log({finDeTurno});
+console.log({listaDeEspecialistas});
