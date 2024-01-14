1359a2cc167a7d41f3ab80f566d68ccbd8f29d88
sazo <sazdev09@gmail.com>
2024-01-14 19:44:20 +0100

    refactor: Mejorar la eficiencia de 'obtenPacientesAsignadosAPediatria'
    Se ha optimizado la función 'obtenPacientesAsignadosAPediatria' para mejorar su eficiencia. Además, se ha añadido la función 'obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios' para obtener pacientes asignados a la especialidad de Pediatría y menores de 10 años.

--- src/motor.ts
+++ src/motor.ts
@@ -5,14 +5,27 @@ const obtenPacientesAsignadosAPediatria = (
     pacientes: Pacientes[]
   ): Pacientes[] => {
     const pacientesDePediatria : Pacientes[] = [];
-    for (let i = 0, j=0; i < pacientes.length; i++) {
-        if (pacientes[i].especialidad.match("Pediatra")) {
+    for (let i = 0, j=0; i < pacientes.length && !pacientes[i].especialidad.match("Pediatra"); i++) {
              pacientesDePediatria[j]= pacientes[i];
              j++;
-        }
     }
     return pacientesDePediatria
   };
 
+  const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+    pacientes: Pacientes[]
+  ): Pacientes[] => {
+    const pacientesPediatriaMenos10 : Pacientes[] = [];
+    for (let i = 0, j=0; i < pacientes.length && pacientes[i].edad < 10; i++) {
+      if (pacientes[i].especialidad.match("Pediatra")) {
+        pacientesPediatriaMenos10[j]= pacientes[i];
+        j++;
+      }
+    }
+    return pacientesPediatriaMenos10;
+  };
+
 const listaDePacientes = obtenPacientesAsignadosAPediatria(pacientes);
+const resultadoMenores10 =obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes);
 console.log({listaDePacientes});
+console.log({resultadoMenores10})
